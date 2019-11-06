c85c5ecac06329687c9419cb488acacaa2807aa1
Evan Plaice <evanplaice@gmail.com>
2019-11-05 21:35:40 -0700

    Add build:docs

---
+++ .config/docs.config.js
@@ -0,0 +1,50 @@
+const docdown = require('docdown');
+const glob = require('glob');
+const { basename, dirname, join } = require('path');
+const { promisify } = require('util');
+const { writeFile } = require('fs');
+const globAsync = promisify(glob);
+
+const srcPath = join(process.cwd(), 'src');
+const docPath = join(process.cwd(), 'docs');
+
+const baseConfig = {
+  toc: 'none',
+  style: 'github',
+  url: `https://github.com/vanillaes/absurdum/doc/README.md`,
+  sourceLink: '',
+  tocHref: '',
+  tocLink: ''
+}
+
+async function match (pattern = '*.js') {
+  return globAsync (pattern, { cwd: srcPath });
+}
+
+function createDoc (baseConfig, path) {
+  // extract names
+  const dir = dirname(path);
+  const file = basename(path);
+  const name = basename(path, '.js');
+  const mdName = `${name}.md`;
+
+  // build the docdown config
+  const config = {
+    title: `${dir}.${name}`,
+    path: join(srcPath, dir, file),
+  }
+  Object.assign(config, baseConfig);
+
+  // create the markdown file
+  const markdown = docdown(config);
+  writeFile(join(docPath, dir, `${name}.md`), markdown, { flag: 'w' }, (err) => {
+    if(err) { throw Error(err); }
+  });
+}
+
+(async () => {
+  const files = await match('**/!(*.spec|index).js');
+  files.forEach(file => createDoc(baseConfig, file));
+})().catch(e => {
+  console.error(e);
+});
